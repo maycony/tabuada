5095dbc325ebba8d7c8612ff4749882b060918c6
Maycon Gabriel | Front End Developer <115764624+maycony@users.noreply.github.com>
2024-11-27 12:27:40 -0300

    Add files via upload

---
+++ tabuada/index.js
@@ -0,0 +1,7 @@
+const number = 7
+
+console.log('Tabuada')
+
+for (let index = 1; index < 11; index++) {
+    console.log(`${number} X ${index} = ` + (index * number))
+}
---
+++ tabuada/readme.md
@@ -0,0 +1,3 @@
+## Tabuada
+
+Crie um programa que, dado um número, imprima a sua tabuada de multiplicação (de 1 a 10).
